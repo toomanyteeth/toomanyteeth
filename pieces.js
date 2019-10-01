5380a0842ef938b5d073e7726fbec792a3496bb7
Meena <meenamakesthings@gmail.com>
2019-09-30 21:46:14 -0700

    basic templating in

---
+++ pieces.js
@@ -0,0 +1,16 @@
+var pieces = {
+    pieces: [
+        {
+            imageURL: "images/plantstellation.png",
+            description: "Under construction!"
+        },
+        {
+            imageURL: "images/it sure is cold out here.png",
+            description: "Under construction!"
+        },
+        {
+            imageURL: "images/at the scarytale ball.png",
+            description: "Under construction!"
+        },
+    ]
+}
