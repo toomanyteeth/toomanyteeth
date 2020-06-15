409cc746424df1c5dbe03cb00c0a2f63d526891f
Meena <meenamakesthings@gmail.com>
2020-06-15 15:22:25 -0700

    layout and images

--- pieces.js
+++ pieces.js
@@ -1,16 +1,87 @@
 var pieces = {
     pieces: [
         {
-            imageURL: "images/plantstellation.png",
-            description: "Under construction!"
+            images: [
+                "images/the scarytale ball - a.png",
+                "images/the scarytale ball - b.png",
+                "images/the scarytale ball - c.png",
+            ],
+            //title: "At The Scarytale Ball",
         },
         {
-            imageURL: "images/it sure is cold out here.png",
-            description: "Under construction!"
+            images: [
+                "images/telugu women - bomber.png",
+                "images/telugu women - puffer.png",
+                "images/telugu women - cardigan.png",
+            ],
+            //title: "Telugu Women Abroad in Wintertime",
         },
         {
-            imageURL: "images/at the scarytale ball.png",
-            description: "Under construction!"
+            images: [
+                "images/yoni ki baat - front.png",
+                "images/yoni ki baat - back.png",
+            ],
+            //title: "Yoni Ki Baat Flyer",
+        },
+        {
+            images: [
+                "images/plantstellation - hands.png",
+                "images/plantstellation - board.png",
+            ],
+            title: "plantstellation",
+            description: "Arrange plants in augmented reality. Play it <a href=\"https:\/\/toomanyteeth.github.io\/plantstellation\/\" target=\"_blank\">here<\/a>."
+        },
+        {
+            images: [
+                "images/it sure is cold out here.gif",
+            ],
+            title: "it sure is cold out here",
+            description: "A short space searching game. Play it <a href=\"https:\/\/toomanyteeth.github.io\/it-sure-is-cold-out-here\/\" target=\"_blank\">here<\/a>."
+        },
+        {
+            images: [
+                "images/endless rainbows - screenshot.png",
+                "images/endless rainbows - installation.jpg",
+            ],
+            title: "endless rainbows",
+            description: "Ride a horse as shirtless Vladimir Putin. Play it <a href=\"https:\/\/toomanyteeth.github.io\/endless-rainbows\/\" target=\"_blank\">here<\/a>."
+        },
+        {
+            images: [
+                "images/avakai - monsoon.jpg",
+                "images/avakai - fall.jpg",
+                "images/avakai - winter.jpg",
+                "images/avakai - summer.jpg",
+            ],
+        },
+        {
+            images: [
+                "images/myth book - the elephant god.png",
+                "images/myth book - the witch and the goddess.png",
+                "images/myth book - the egg came first.png",
+                "images/myth book - flute.png",
+                "images/myth book - a heavenly war.png",
+                "images/myth book - ramayana.png",
+            ],
+            //title: "Mythology Illustrations",
+            description: "Illustrations for <a href=\"https:\/\/www.goodreads.com\/book\/show\/52050947-love-pride-virtue-and-fate\" target=\"_blank\">Love, Pride, Virtue, and Fate by Bharat Krishan<\/a>"
+        },
+        {
+            images: [
+                "images/sad dessert - ice cream.png",
+                "images/sad dessert - donut.png",
+                "images/sad dessert - cake.png",
+            ],
+        },
+        {
+            images: [
+                "images/misc - xwp map.png",
+                "images/misc - donut cat.png",
+                "images/misc - devi.png",
+                "images/misc - iso plants.png",
+                "images/plantstellation - board.png",
+            ],
+            //title: "Miscellaneous Illustrations"
         },
     ]
 }
