e8668593464d7fe8ec23453fdb6ae9d9dbd0e328
Meena <meenamakesthings@gmail.com>
2021-09-02 18:29:27 -0700

    made a separate design page

---
+++ design pieces.js
@@ -0,0 +1,77 @@
+var pieces = {
+    pieces: [
+        {
+            videos: [
+                "design images/nrdb explainer video",
+            ],
+            title: "New Relic NRDB Explainer Video",
+            description: "Created assets and animated video using New Relic's style."
+        },
+        {
+            videos: [
+                "design images/calendly rally week video",
+            ],
+            title: "Calendly Rally Week Video",
+            description: "Animated video using Calendly's style."
+        },
+        {
+            images: [
+                "design images/tanium/1.png",
+                "design images/tanium/2.png",
+                "design images/tanium/3.png",
+                "design images/tanium/4.png",
+                "design images/tanium/5.png",
+                "design images/tanium/6.png",
+                "design images/tanium/7.png",
+                "design images/tanium/8.png",
+            ],
+            title: "Tanium Explainer Video Storyboards",
+            description: "Storyboarded explainer video and created visual metaphors for security breaches and restoration."
+        },
+        {
+            videos: [
+                "design images/redis/redis labs clip",
+            ],
+            images: [
+                "design images/redis/1.png",
+                "design images/redis/2.png",
+                "design images/redis/3.png",
+            ],
+            title: "Redis Explainer Video Storyboards and Animation",
+            description: "Storyboarded explainer video, created particle system and animated some portions."
+        },
+        {
+            videos: [
+                "design images/snowflake explainer",
+            ],
+            title: "Snowflake Explainer Video",
+            description: "Animated video using Snowflake's style."
+        },
+        {
+            videos: [
+                "design images/confluent clip",
+            ],
+            title: "Confluent SKO Animation",
+            description: "Created animated clips for Confluent's SKO event, using Cinema 4D to animate in 3D."
+        },
+        {
+            videos: [
+                "design images/snowflake globe",
+            ],
+            title: "Snowflake Summit Animation",
+            description: "Created this animated clip, among others, for Snowflake's Summit event."
+        },
+        {
+            images: [
+                "design images/oneconcern/oneconcern title slide 1.png",
+                "design images/oneconcern/oneconcern title slide 2.png",
+                "design images/oneconcern/oneconcern title slide 3.png",
+                "design images/oneconcern/oneconcern title slide 4.png",
+                "design images/oneconcern/oneconcern slide 1.png",
+                "design images/oneconcern/oneconcern slide 2.png",
+            ],
+            title: "OneConcern Slides",
+            description: "Created slides, diagrams, and composited art for OneConcern's corporate pitch deck."
+        },
+    ]
+}
