0f7bda0064605363eb0c1ecb3d01578f7c9c7e71
Meenakshi <meenamakesthings@gmail.com>
2026-03-10 13:03:44 -0700

    new samples

--- event.js
+++ event.js
@@ -1,10 +1,12 @@
 var info = {
     pageTitle: "Event Presentations",
-    pageDescription: "I've worked on many mainstage presentations, breakout decks, and promotional videos for events in tech. Events I've done work for include AWS re:Invent, Snowflake Summit, Salesforce's Dreamforce, and internal events at New Relic and PagerDuty, all across multiple years. Some of my capabilities include designing for venues with multiple screens, creating motion graphics pieces to elevate key moments, and providing onsite support for rehearsals and updates.\nI want to respect NDAs, so I've recreated some of the work I've done with dummy information and branding to show here. (WIP)",
+    pageDescription: "I've worked on many mainstage presentations, breakout decks, and promotional videos for events in tech. Events I've done work for include AWS re:Invent, Snowflake Summit, Salesforce's Dreamforce, and internal events at New Relic and PagerDuty, all across multiple years. Some of my capabilities include designing for venues with multiple screens, creating motion graphics pieces to elevate key moments, and providing onsite support for rehearsals and updates.\nI want to respect NDAs, so I've recreated some of the work I've done with dummy information and branding to show here (WIP). I animated these slides using only PowerPoint.",
     pieces: [
         {
             videoloops: [
                 "images/slide animation samples/timeline",
+                "images/slide animation samples/lake",
+                "images/slide animation samples/delta",
                 "images/slide animation samples/roadmap",
                 "images/slide animation samples/dot graph",
                 "images/slide animation samples/flowchart",
